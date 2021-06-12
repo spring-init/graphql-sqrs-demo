580b90ba997c5851b2210375ad132a2d0a5c4e5e
Gabriel Paz <jgabrielpazs@gmail.com>
2021-06-12 11:33:36 -0500

    GP-1 Testing with oauth security

--- src/test/groovy/mx/gps/graphqlsqpr/api/UserControllerCmpTest.groovy
+++ src/test/groovy/mx/gps/graphqlsqpr/api/UserControllerCmpTest.groovy
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.BodyInserters
@@ -18,6 +19,7 @@ import spock.lang.Stepwise
 @AutoConfigureWebTestClient
 @ActiveProfiles("module-test")
 @Stepwise
+@WithMockUser
 class UserControllerCmpTest extends Specification {
     @Autowired
     private WebTestClient webClient
